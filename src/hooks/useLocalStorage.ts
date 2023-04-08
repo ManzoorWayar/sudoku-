f021c0976066553aa6a36b62c7524649fc5fc079
manzoorWayar <softwareengineerwayar@gmail.com>
2023-04-08 16:36:22 +0430

    fix the intial value bug

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react';
 
-const getSavedValue = (key:any, initialValue:any) => {
-    const savedValue = JSON.parse(localStorage.getItem(key) || '{}') ;
+const getSavedValue = (key: any, initialValue: any) => {
+    const savedValue = JSON.parse(localStorage.getItem(key) || '{}');
 
     if (savedValue) return savedValue;
- 
+
     if (initialValue instanceof Function) return initialValue();
 
     return initialValue;
 }
 
-const useLocalStorage = (key:any, initialValue:any) => {
+const useLocalStorage = (key: any, initialValue: any) => {
     const [value, setValue] = useState(() => {
         return getSavedValue(key, initialValue);
     });
@@ -19,6 +19,8 @@ const useLocalStorage = (key:any, initialValue:any) => {
         localStorage.setItem(key, JSON.stringify(value));
         localStorage.setItem('clickValue', '1')
     }, [key, value])
+
+    localStorage.setItem('clickValue', '1')
 
     return [value, setValue];
 }
